0b4c2ea2e43fa698d11ac724ed166e85ae7f3fbb
ajaykumarshakya560@gmail.com <ajaykumarshakya560@gmail.com>
2025-03-03 17:44:55 +0530

    Banner modified

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,9 @@
 import { Helmet } from 'react-helmet-async';
-import woman from "../assets/img/woman.png";
-import Flowerpic from '../assets/img/flower.png';
+import { Carousel } from 'antd';
 import '../assets/styles/style.css';
+import img1 from "../assets/img/Default.jpg";
+import img2 from "../assets/img/Variant2.jpg";
+import img3 from '../assets/img/Variant3.jpg';
 
 const Hero = () => {
   return (
@@ -10,33 +12,29 @@ const Hero = () => {
         <title>Fanapo - Premium Feminine Hygiene Products</title>
         <meta name="description" content="Embrace each cycle with our pads that offer unmatched confidence and comfort" />
       </Helmet>
-      <section className="relative hero-section min-h-screen  flex flex-col">
-        <div className="max-w-7xl mx-auto px-4 md:px-0">
-          <header className="flex items-center justify-between md:px-4  pt-8">
-            <h2 className="text-4xl md:text-6xl logo font-bold text-[#D70283]">FANAPO</h2>
-            <button className="bg-[#D70283] text-white px-6 py-2 rounded-full hover:bg-[#D81B60] transition-colors">
-              Contact Us
-            </button>
-          </header>
-          <div className="container mx-auto  py-16 flex flex-col md:flex-row items-center justify-between flex-grow">
-            <div className=" relative md:w-1/2 mb-8 md:mb-0">
-              <img src={Flowerpic} className='absolute right-5 md:top-[-40%] md:right-[35%] w-20 md:w-40 h-auto' alt="flower" />
-              <h1 className="text-4xl md:text-7xl font-bold mb-6 ">
-                Enfold <br /> the cycle with confidence
-              </h1>
-
-              <p className="text-lg mb-8">
-                Embrace each cycle with our pads that offer <br />
-                unmatched confidence and comfort.
-              </p>
-
-            </div>
-            <div className="md:w-1/2  flex justify-end ">
-              <img
-                src={woman}
-                alt="Woman pointing"
-                className=" w-[600px]"
-              />
+      <section className="relative hero-section md:min-h-screen flex flex-col">
+        <div className="w-full">
+          <div className="max-w-7xl mx-auto px-4 md:px-0">
+            <header className="flex items-center justify-between md:px-4 pb-4 pt-8">
+              <h2 className="text-4xl md:text-6xl logo font-bold text-[#D70283]">FANAPO</h2>
+              <button className="bg-[#D70283] text-white px-6 py-2 rounded-full hover:bg-[#D81B60] transition-colors">
+                Contact Us
+              </button>
+            </header>
+          </div>
+          <div className="container mx-auto flex flex-col items-center flex-grow">
+            <div className="w-full  mx-auto">
+              <Carousel autoplay className="w-full">
+                <div className="md:h-screen flex justify-center">
+                  <img src={img1} alt="Variant 1" className="w-full  md:h-full object-cover" />
+                </div>
+                <div className="md:h-screen flex justify-center">
+                  <img src={img2} alt="Variant 2" className="w-full  md:h-full object-cover" />
+                </div>
+                <div className="md:h-screen flex justify-center">
+                  <img src={img3} alt="Variant 3" className="w-full  md:h-full object-cover" />
+                </div>
+              </Carousel>
             </div>
           </div>
         </div>
